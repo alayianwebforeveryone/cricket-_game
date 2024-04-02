daade9fdd53c914673ec28e3689ca408a7de779a
alayianwebforeveryone <alayiansaeed@gmail.com>
2024-04-02 06:44:51 +0500

    add files

---
+++ index.js
@@ -0,0 +1,46 @@
+// variable
+// input 
+// output
+// compare etc
+// operater and its types 
+// datatypes
+// condition 
+// loops
+
+// three ways to define variable in js 
+
+
+// var, let , const
+
+
+//var x = 10  // old way to create variable 
+//let y = 10
+//const s = "something"
+
+
+
+//console.log("y")
+//const d ="Affan"
+//let e ="two"
+// console.log("Affan Saeed")
+
+// const fname = "Afan"
+// const lname = "Saeed"
+
+// console.log("My name is :", fname + lname )
+//let i ="two"
+//const p ="six"
+//console.log("Hellow World",i +" "+p)
+//onsole.log()
+let v ="10"
+const y="89"
+const t = 25;
+let x = 6;
+const devide=x/t;
+console.log("devide",devide)
+const modulus=t%x;
+console.log("modulas",modulus)
+  const sum = x +t
+  const multiply = x*t
+  console.log("multiply", multiply)
+  console.log( "sum",sum)
